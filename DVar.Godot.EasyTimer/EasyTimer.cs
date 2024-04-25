fe7bb15f97397025b5646ced98d5235e546d1017
Danpeo <danilvar4@gmail.com>
2024-04-25 11:39:04 +0400

    Init

---
+++ DVar.Godot.EasyTimer/EasyTimer.cs
@@ -0,0 +1,27 @@
+﻿using Godot;
+using Timer = Godot.Timer;
+
+namespace DVar.Godot.EasyTimer;
+
+public class EasyTimer
+{
+    public Timer Timer { get; }
+
+    public EasyTimer(Node node, Action timeOut, float waitTime = 1.0f, bool oneShot = true)
+    {
+        Timer = new Timer();
+        Timer.OneShot = oneShot;
+        Timer.WaitTime = waitTime;
+        node.AddChild(Timer);
+        Timer.Timeout += timeOut;
+    }
+
+    public void Start() => Timer.Start();
+
+    public void Stop()
+    {
+        Timer.Stop();
+        if (Timer.OneShot)
+            Timer.QueueFree();
+    }
+}
